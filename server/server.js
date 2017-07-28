859b2e04ac1f108e7e9f5cbf315f8f0a26233632
Ygor Duarte Lemos Pereira <vntygpe@venturus.local>
2017-07-28 15:10:19 -0300

    Add route for get todo by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var parser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
@@ -27,6 +28,22 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){        
+        return res.status(404).send('Invalid id');
+    }    
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send(e);
+    });        
 });
 
 app.listen(3000, () => {
