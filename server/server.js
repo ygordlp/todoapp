4d393adc8a2169f7ff161f207250e861a30c3e9e
Ygor Duarte Lemos Pereira <vntygpe@venturus.local>
2017-07-28 13:25:59 -0300

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var parser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/User');
+
+var app = express();
+
+app.use(parser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) =>{
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) =>{
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server started at 3000');
+});
+
+module.exports = {app};
