11ac57027fcef1e77604a13857bb20fb0487e24a
Ygor Duarte Lemos Pereira <vntygpe@venturus.local>
2017-07-31 13:12:28 -0300

    Add test for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -321,6 +321,24 @@ describe('POST /users/login', () => {
     });
 });
 
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', testUsers[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if(err){
+                    done(err);
+                }
+
+                User.findById(testUsers[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
 
 
 
