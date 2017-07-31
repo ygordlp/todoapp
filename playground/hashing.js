2b8c9d16e380c83101790446eab85041b5d68852
Ygor Duarte Lemos Pereira <vntygpe@venturus.local>
2017-07-31 08:02:47 -0300

    Hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,15 @@
 const {SHA256} = require('crypto-js');
+const bcrypt = require('bcryptjs');
 
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
 
-console.log('Message:', message);
-console.log('Hash:', hash);
+// console.log('Message:', message);
+// console.log('Hash:', hash);
 
-var data = {
-    id: 4
-};
+// var data = {
+//     id: 4
+// };
 
 // var token = {
 //     data: data, 
@@ -27,10 +28,22 @@ var data = {
 // }
 
 //WITH JWT
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-var token = jwt.sign(data, '123adbc');
-console.log('JWT:', token);
+// var token = jwt.sign(data, '123adbc');
+// console.log('JWT:', token);
 
-var decoded = jwt.verify(token, '123adbc');
-console.log('Decoded:', decoded);
+// var decoded = jwt.verify(token, '123adbc');
+// console.log('Decoded:', decoded);
+
+var password = '123abc';
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log('Bcrypt hash', hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$IclPe0oDoOP/1sBeErXwSeT9vomntyAAk4VV2LSiKQS/DLOnTPVhe';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log('Is ok', res);
+});
